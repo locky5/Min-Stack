4a416f5a3c46c58bd8674c746f94d774c4d1984d
locky5 <daikevin19@yahoo.com>
2020-01-02 20:15:14 -0500

    done

---
+++ minstack.js
@@ -0,0 +1,35 @@
+/**
+ * initialize your data structure here.
+ */
+var MinStack = function() {
+    this.arr = []
+};
+
+/**
+ * @param {number} x
+ * @return {void}
+ */
+MinStack.prototype.push = function(x) {
+    this.arr.push(x)
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    return this.arr.pop()
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    return this.arr[this.arr.length - 1]
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+    return Math.min(...this.arr)
+};
